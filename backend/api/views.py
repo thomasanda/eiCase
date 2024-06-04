bd7710b199c65b86e600de7185ac6baa79ef4276
Thomas Anda <thomas.anda@oliasoft.com>
2024-06-04 19:19:20 +0200

    get property by portfolio-id

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,5 +9,18 @@ class PortfolioViewSet(viewsets.ModelViewSet):
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
-    queryset = Property.objects.all()
     serializer_class = PropertySerializer
+    queryset = Property.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        portfolio_id_str = self.request.query_params.get("portfolio_id")
+        if portfolio_id_str is not None:
+            cleaned_portfolio_id_str = "".join(
+                filter(str.isdigit, portfolio_id_str)
+            )  # Keep only digits
+            portfolio_id = int(cleaned_portfolio_id_str)  # Convert to integer
+            queryset = queryset.filter(portfolio_id=portfolio_id)
+
+        return queryset
